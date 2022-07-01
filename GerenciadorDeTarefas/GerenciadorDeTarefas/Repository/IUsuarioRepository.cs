41a314314a83fba06af9a8f8a2bbab8cbae11278
yazawadesu <brengcomputacao@gmail.com>
2022-07-01 19:01:53 -0300

    first commit

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Repository/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using GerenciadorDeTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Repository
+{
+    public interface IUsuarioRepository
+    {
+        public void Salvar(Usuario usario);
+        bool ExisteUsuarioPeloEmail(string email);
+        Usuario GetUsuarioByLoginSenha(string login, string senha);
+        Usuario GetById(int idUsuario);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# gerenciador-tarefas-c-sharp
+API Backend para o projeto de Gerenciamento de Tarefas referente ao teste prático para empresa Ploomes
